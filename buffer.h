c2a17926772d9669bb7944de9b44505c598532d5
Ben Hoppert <benhoppert@gmail.com>
2026-07-27 23:58:42 +0200

    renamed buffer.h and buffer.c to editor.h/c (not finished renaming everything)

--- buffer.h
+++ buffer.h
@@ -1,5 +1,5 @@
-#ifndef BUFFER_H
-#define BUFFER_H
+#ifndef EDITOR_H
+#define EDITOR_H
 
 #include <stdlib.h>
 
@@ -13,4 +13,16 @@ void line_insert_text_before(Line *line, const char *text, size_t col);
 void line_backspace(Line *line, size_t col);
 void line_delete(Line *line, size_t col);
 
-#endif
+typedef struct {
+	size_t capacity;
+	size_t size;
+	Lines *lines;
+	size_t cursor_row;
+	size_t cursor_col;
+} Editor;
+
+void editor_insert_text_before_cursor(Editor *editor, const char *text);
+void editor_backspace(Editor *editor);
+void editor_delete(Editor *editor);
+
+#endif // EDITOR_H
